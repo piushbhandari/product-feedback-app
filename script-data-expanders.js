c8072aaedeac6880f5e00822a070e189fd783d8e
piush.bhandari <piush.bhandari@americaneagle.com>
2023-03-03 17:28:19 -0600

    added responsiveness to homepage

---
+++ script-data-expanders.js
@@ -0,0 +1,18 @@
+const dataTogglers = [...document.querySelectorAll("[data-expander]")];
+
+dataTogglers.forEach((toggle) => {
+  toggle.addEventListener("click", (e) => {
+    e.preventDefault();
+    const currentBtn = e.currentTarget;
+    const btnAttribute = currentBtn.getAttribute("data-expander-toggle");
+    const targetElement = document.getElementById(btnAttribute);
+
+    if (!currentBtn.classList.contains("active")) {
+      currentBtn.classList.add("active");
+      targetElement.classList.add("active");
+    } else {
+      currentBtn.classList.remove("active");
+      targetElement.classList.remove("active");
+    }
+  });
+});
